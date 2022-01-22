f09c5238feb8c2350cb233633b778adf4c76c199
emzei <emzei.j@gmail.com>
2022-01-22 23:45:49 +0900

    Remove randomBg color

--- js/backgroundImg.js
+++ js/backgroundImg.js
@@ -1,5 +1,3 @@
 const bgImgs = [ "0.jpeg", "1.jpeg", "2.jpeg"];
 const img = bgImgs[Math.floor(Math.random() * bgImgs.length)];
-const backgroundImage = document.createElement("img");
-backgroundImage.src = `./img/${img}`;
-document.body.appendChild(backgroundImage);
+document.body.style.backgroundImage = `url('./img/${img}')`;
