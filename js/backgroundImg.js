f7eb668a7a8e806009b7f35079112097159d4310
emzei <emzei.j@gmail.com>
2022-01-22 12:28:44 +0900

    Fix - remove img file name in js

--- js/backgroundImg.js
+++ js/backgroundImg.js
@@ -1,4 +1,4 @@
-const bgImgs = [ "0.jpeg", "1.jpeg", "2.jpeg","3.jpeg"];
+const bgImgs = [ "0.jpeg", "1.jpeg", "2.jpeg"];
 const img = bgImgs[Math.floor(Math.random() * bgImgs.length)];
 const backgroundImage = document.createElement("img");
 backgroundImage.src = `./img/${img}`;
